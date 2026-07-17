0596c1954a01e547e94e0b1a8ac6906296dbfa62
Tom McAndrews <tmcandrews3@gmail.com>
2026-07-17 17:09:56 -0400

    Add /test-order route for safe testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import BrickFinder from './pages/BrickFinder';
 import Admin from './pages/Admin';
+import TestOrder from './pages/TestOrder';   // ← Add this line
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<BrickFinder />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/test-order" element={<TestOrder />} />
     </Routes>
   );
 }
